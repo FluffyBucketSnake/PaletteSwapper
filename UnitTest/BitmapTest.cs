c018f311b67a416bcc7513dbca09466ad50d19a9
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2021-03-20 15:00:08 -0400

    Add Bitmap unit test

---
+++ UnitTest/BitmapTest.cs
@@ -0,0 +1,26 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace PaletteSwapper.UnitTest
+{
+    [TestClass]
+    public class BitmapTest
+    {
+        public static Colour[] bitmap0Data = new Colour[] 
+        {
+            Colour.White,
+            Colour.Red,
+            Colour.Green,
+            Colour.Blue
+        };
+
+        [TestMethod]
+        [DeploymentItem("bitmap0.png")]
+        public void Load()
+        {
+            {
+                ImageBitmap bitmap = ImageBitmap.FromFile("bitmap0.png");
+                CollectionAssert.AreEqual(bitmap0Data, bitmap.GetData());
+            }
+        }
+    }
+}
