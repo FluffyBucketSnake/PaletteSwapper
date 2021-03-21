89ef726df3e092d70eb926cb9320720a9ad1e305
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2021-03-20 21:30:29 -0400

    Add more cases for BitmapTest

--- UnitTest/BitmapTest.cs
+++ UnitTest/BitmapTest.cs
@@ -5,12 +5,20 @@ namespace PaletteSwapper.UnitTest
     [TestClass]
     public class BitmapTest
     {
-        public static Colour[] bitmap0Data = new Colour[] 
+        public static Colour[] bitmap0Data0 = new Colour[]
         {
             Colour.White,
             Colour.Red,
             Colour.Green,
             Colour.Blue
+        };
+
+        public static Colour[] bitmap0Data1 = new Colour[]
+        {
+            Colour.Black,
+            Colour.Blue,
+            Colour.Red,
+            Colour.Green
         };
 
         [TestMethod]
@@ -19,8 +27,40 @@ namespace PaletteSwapper.UnitTest
         {
             {
                 ImageBitmap bitmap = ImageBitmap.Load("bitmap0.png");
-                CollectionAssert.AreEqual(bitmap0Data, bitmap.GetData());
+                CollectionAssert.AreEqual(bitmap0Data0, bitmap.GetData());
             }
+        }
+
+        [TestMethod]
+        [DeploymentItem("bitmap0.png")]
+        [DeploymentItem("bitmap1.png")]
+        public void ToIndexedBitmap()
+        {
+            Palette[] palettes = Palette.Load("bitmap1.png");
+            ImageBitmap source = ImageBitmap.Load("bitmap0.png");
+
+            IndexedBitmap result = IndexedBitmap.FromBitmap(source, palettes[0]);
+            CollectionAssert.AreEqual(result.GetData(), new int[] { 0, 1, 2, 3 });
+
+            Assert.ThrowsException<System.Collections.Generic.KeyNotFoundException>(() =>
+            {
+                IndexedBitmap.FromBitmap(source, palettes[1]);
+            });
+        }
+
+        [TestMethod]
+        [DeploymentItem("bitmap0.png")]
+        [DeploymentItem("bitmap1.png")]
+        public void SwapPalette()
+        {
+            Palette[] palettes = Palette.Load("bitmap1.png");
+            ImageBitmap source = ImageBitmap.Load("bitmap0.png");
+
+            IndexedBitmap intermidiate = IndexedBitmap.FromBitmap(source, palettes[0]);
+            intermidiate.Palette = palettes[1];
+
+            ImageBitmap result = intermidiate.Dereference();
+            CollectionAssert.AreEqual(result.GetData(), bitmap0Data1);
         }
     }
 }
