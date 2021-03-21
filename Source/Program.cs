5e576acadc3375a1d232b4c91998d78c99472065
Bruno Henrique Glowaski Morais <bruno.glowaski@outlook.com>
2021-03-20 22:07:13 -0400

    Add program code

--- Source/Program.cs
+++ Source/Program.cs
@@ -7,7 +7,64 @@ namespace PaletteSwapper
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Args _args = null;
+            try
+            {
+                _args = Args.Parse(0, args);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Argument error: {0}", e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("Argument format error: {0}", e.Message);
+            }
+            if (_args == null) return;
+
+            // Load source bitmap.
+            ImageBitmap source = null;
+            try
+            {
+                source = ImageBitmap.Load(_args.SourcePath);
+            }
+            catch (UnknownImageFormatException e)
+            {
+                Console.WriteLine("Can't load source image: unknown image format.");
+                Console.WriteLine("Details: {0}", e.Message);
+            }
+            catch (InvalidImageContentException e)
+            {
+                Console.WriteLine("Can't load source image: file has invalid content.");
+                Console.WriteLine("Details: {0}", e.Message);
+            }
+            if (source == null) return;
+
+            // Load palettes.
+            Palette[] palettes = null;
+            try
+            {
+                palettes = Palette.Load(_args.PaletteTablePath);
+            }
+            catch (UnknownImageFormatException e)
+            {
+                Console.WriteLine("Can't load palettes: unknown format.");
+                Console.WriteLine("Details: {0}", e.Message);
+            }
+            catch (InvalidImageContentException e)
+            {
+                Console.WriteLine("Can't load palettes: file has invalid content.");
+                Console.WriteLine("Details: {0}", e.Message);
+            }
+            if (palettes == null) return;
+
+            // Palette swapping code
+            var indexed = IndexedBitmap.FromBitmap(source, palettes[_args.SourcePaletteIndex]);
+            indexed.Palette = palettes[_args.TargetPaletteIndex];
+            var output = indexed.Dereference();
+
+            // Save output.
+            output.Save(_args.DestinationPath);
         }
     }
 }
